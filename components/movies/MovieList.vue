a30efed7f95b11fb16ac7ed91651db7138e91bb3
JasonTongg <aboytong123@gmail.com>
2023-01-19 21:07:46 +0700

    done

---
+++ components/movies/MovieList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row featured__filter">
+    <!-- Component Item Here -->
+    <app-movie-item :data="data"></app-movie-item>
+  </div>
+</template>
+<script>
+import MovieItem from "./MovieItem.vue";
+
+export default {
+  props: ["data"],
+  data() {
+    return {
+      data: this.data,
+    };
+  },
+  components: {
+    "app-movie-item": MovieItem,
+  },
+};
+</script>
+<style scoped>
+.featured__filter {
+  min-width: 300px;
+}
+</style>
